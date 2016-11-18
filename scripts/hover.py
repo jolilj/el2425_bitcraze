ade9d51cddcaa2da20c8b7757c4b0187d311f9d0
Joakim Lilja <lilja.joakim@gmail.com>
2016-11-18 13:33:23 +0100

    sneak peak at hovering, updated readme

---
+++ scripts/hover.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import rospy
+from std_srvs.srv import Empty
+
+# Enable the takeoff service. This assumes the crazyflie position is accurately estimated by the LPS system. It might take a few seconds for the ekf to converge so wait before running this
+
+if __name__ == "__main__":
+	rospy.loginfo("waiting for takeoff service")
+	rospy.wait_for_service('/crazyflie/takeoff')
+	rospy.loginfo("found takeoff service")
+	takeoff = rospy.ServiceProxy('/crazyflie/takeoff', Empty)
+	takeoff()
