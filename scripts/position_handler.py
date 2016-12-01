2b98ba62e8f233d652c44232809e0944a991c06a
Joakim Lilja <lilja.joakim@gmail.com>
2016-12-01 13:25:39 +0100

    added generateTrajectory

--- scripts/position_handler.py
+++ scripts/position_handler.py
@@ -16,6 +16,8 @@ class PositionHandler:
         self.msg.pose.position.y = 0
         self.msg.pose.position.z = 0.1
 
+	self.trajectory = [[],[],[]]
+
         yaw = 0
         quaternion = tf.transformations.quaternion_from_euler(0, 0, yaw)
         self.msg.pose.orientation.x = quaternion[0]
@@ -25,14 +27,42 @@ class PositionHandler:
 
         self.rate = rospy.Rate(30)
         self.pub = rospy.Publisher("/crazyflie/goal", PoseStamped, queue_size=1)
-        self.setGoalService = rospy.Service('/crazyflie/setgoal', SetGoal, self.updateGoal)
+        self.setGoalService = rospy.Service('/crazyflie/setgoal', SetGoal, self.goalServiceCallback)
         self.isRunning = False
 
-    def updateGoal(self, req):
+    def generateLinearTrajectory(self, x, y, z):
+	x_old = self.msg.pose.position.x
+	y_old = self.msg.pose.position.y
+	z_old = self.msg.pose.position.z
+	
+	if x == x_old :
+	    kx = 0
+	else: 
+	    Tx = 5*(x - x_old)
+	    kx = (x - x_old)/Tx
+	if y == y_old :
+	    ky = 0
+	else:
+	    Ty = 5*(y - y_old)
+	    ky = (y - y_old)/Ty
+	if z == z_old :
+	    kz = 0
+	else:
+	    Tz = 5*(z - z_old)
+	    kz = (z - z_old)/Tz
+
+	for i in range(1, Tx+1):
+	    self.trajectory.x.append([x_old + kx*i])
+	for i in range(1, Ty+1):
+	    self.trajectory.y.append([y_old + ky*i])
+	for i in range(1, Tz+1):
+	    self.trajectory.z.append([z_old + kz*i])
+	
+	
+
+    def goalServiceCallback(self, req):
         print "Msg received: [x: %f, y: %f, z: %f]\n" %(req.x, req.y, req.z)
-        self.msg.pose.position.x = req.x
-        self.msg.pose.position.y = req.y
-        self.msg.pose.position.z = req.z
+        self.generateLinearTrajectory(req.x, req.y, req.z)
         
         return()
 
