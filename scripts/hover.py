f78bfb07b4aff35cddac1f8e66ec8d7e5b9917a4
Joakim Lilja <lilja.joakim@gmail.com>
2016-12-01 10:57:45 +0100

    tweaked hover script to take initial goal position as argument

--- scripts/hover.py
+++ scripts/hover.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python
 
+import sys
 import rospy
 from std_srvs.srv import Empty
+from el2425_bitcraze.src import SetGoal
 
 # Enable the takeoff service. This assumes the crazyflie position is accurately estimated by the LPS system. 
 # It might take a few seconds for the ekf to converge so wait before running this
 
 if __name__ == "__main__":
-    rospy.loginfo("waiting for takeoff and landing service")
+    # Wait for services to become available
+    rospy.loginfo("waiting for services")
     rospy.wait_for_service('/crazyflie/takeoff')
     rospy.wait_for_service('/crazyflie/land')
-    rospy.loginfo("found takeoff and landing service")
-
+    rospy.wait_for_service('/crazyflie/setgoal')
+    rospy.loginfo("found services")
+    
+    # Service call functions
     takeoff = rospy.ServiceProxy('/crazyflie/takeoff', Empty)
     land = rospy.ServiceProxy('/crazyflie/land', Empty)
+    setgoal = rospy.ServiceProxy('/crazyflie/setgoal', SetGoal)
+    
+    # Parse goal from terminal
+    try:
+        x = sys.argv[1]
+        y = sys.argv[2]
+        z = sys.argv[3]
+    except:
+        print('Please specify hover coordinates, e.g.: hover.py -1.0 1.0 1.0') 
 
+    # Set initial goal position and takeoff!
+    setgoal(x,y,z)
     takeoff()
+
+    #Wait for user to press any key and then land
     raw_input("Press any key to land")
     land()
