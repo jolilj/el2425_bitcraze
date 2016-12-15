0ee5beec052a629761d2844bb00554118679bba3
Joakim Lilja <lilja.joakim@gmail.com>
2016-12-15 17:17:56 +0100

    pipe trajectories through collision avoidance algorithm

--- scripts/position_handler.py
+++ scripts/position_handler.py
@@ -22,7 +22,21 @@ from std_msgs.msg import Float32MultiArray as Array
 deltaT = 0.2    # 0.2
 vel = 0.5
 class PositionHandler:
-    def __init__(self, x, y, z):
+    def __init__(self):
+        self.isSingleCF = 0
+        namespace = rospy.get_namespace()
+        if namespace == "/crazyflie/":
+            self.isSingleCF = 1
+        elif namespace == "/crazyflie0/":
+            otherNamespace = "/crazyflie1/"
+        else:
+            otherNamespace = "/crazyflie0/"
+
+        x = rospy.get_param("x")
+        y = rospy.get_param("y")
+        z = rospy.get_param("z")
+
+
         # The msg that will be published on the goal topic
         self.referenceMsg = PoseStamped()
         self.referenceMsg.header.seq = 0
@@ -31,7 +45,7 @@ class PositionHandler:
         self.referenceMsg.pose.position.x = x
         self.referenceMsg.pose.position.y = y
         self.referenceMsg.pose.position.z = z
-
+        
         self.targetPosition = [x,y,z]
         self.deltaT = deltaT   # 0.2
 
@@ -43,12 +57,23 @@ class PositionHandler:
         self.referenceMsg.pose.orientation.w = quaternion[3]
 
         self.rate = rospy.Rate(20)
+
         self.goalPub = rospy.Publisher("goal", PoseStamped, queue_size=1)
         self.targetPosPub = rospy.Publisher("target_pos", Array, queue_size=1, latch=True)
 
         rospy.Service('set_target_position', SetTargetPosition, self.targetPositionServiceCallback)
-
+        
         self.isRunning = False
+
+        if not self.isSingleCF:
+            x2 = rospy.get_param(otherNamespace + "x")
+            y2 = rospy.get_param(otherNamespace + "y")
+            z2 = rospy.get_param(otherNamespace + "z")
+
+            self.cf2Pos = [x2, y2, z2]
+            self.cf2PrevPos = [x2, y2, z2]
+
+            rospy.Subscriber(otherNamespace + "goal", PoseStamped, self.cf2GoalCallback)
    
     # ====== Target Position Service Callback ==== 
     # When the target_position service is called we want to
@@ -57,7 +82,7 @@ class PositionHandler:
     #     * update the internal target position variable used when calculating the intermediate goals 
     def targetPositionServiceCallback(self, req):
         self.targetPosition = [req.x, req.y, req.z]
-	msg = Array()
+        msg = Array()
         msg.data = self.targetPosition
         self.targetPosPub.publish(msg)
         return()
@@ -65,26 +90,27 @@ class PositionHandler:
     # ===== Interpolator ====
     # Based on previous goal position this function publishes a new goal incremented in the direction towards the target position
     # The incrementation is determined by the parameter k. 
+    # The second crayflies position and direction is also passed into the collision avoidance algorithm
+
     def calculatePIDReferenceGoal(self):
         x_old = self.referenceMsg.pose.position.x
         y_old = self.referenceMsg.pose.position.y
         z_old = self.referenceMsg.pose.position.z
 
-        delta_x = self.targetPosition[0]-x_old
-        delta_y = self.targetPosition[1]-y_old
-        delta_z = self.targetPosition[2]-z_old
-        
-        delta_norm = (math.sqrt(math.pow(delta_x,2)+math.pow(delta_y,2)+math.pow(delta_z,2)))
-        k = 0
-        if not delta_norm == 0:
-            k = vel*self.deltaT/delta_norm
+        cfDirection = self.calcDirection(self.targetPosition,[x_old, y_old, z_old])
 
-        x_new = x_old+k*delta_x
-        y_new = y_old+k*delta_y
-        z_new = z_old+k*delta_z
-	  
+        k = vel*self.deltaT
+
+        nextPos = [x_old+k*cfDirection[0], y_old+k*cfDirection[1],x_old+k*cfDirection[2]]
+        
+        if not self.isSingleCF:
+            cf2Direction = self.calcDirection(self.cf2Pos, self.cf2PrevPos)
+            cf2NextPos = [self.cf2Pos[0]+k*cf2Direction[0], self.cf2Pos[1]+k*cf2Direction[1],self.cf2Pos[2]+k*cf2Direction[2]]
+            #nextPos = collisionAvoidance(nextPos, cfDirection, cf2NextPos, cf2Direction)
+            self.cf2PrevPos = self.cf2Pos
+
         #If overshoot set new position to goal
-        if math.fabs(self.targetPosition[0]-x_old) < math.fabs(k*delta_x):
+        if math.fabs(self.targetPosition[0]-x_old) <= math.fabs(k*cfDirection[0]):
             x_new = self.targetPosition[0]
             y_new = self.targetPosition[1]
             z_new = self.targetPosition[2]
@@ -92,6 +118,25 @@ class PositionHandler:
         self.referenceMsg.pose.position.x = x_new
         self.referenceMsg.pose.position.y = y_new
         self.referenceMsg.pose.position.z = z_new
+        
+
+    # ====== Callback for second crazyflie goal =====
+    # ===============================================
+
+    def cf2GoalCallback(self,goal):
+        self.cf2Pos = [goal.pose.position.x, goal.pose.position.y, goal.pose.position.z]
+
+    def calcDirection(self, pos1, pos2):
+        delta_x = float(pos1[0]-pos2[0])
+        delta_y = float(pos1[1]-pos2[1])
+        delta_z = float(pos1[2]-pos2[2])
+        
+        delta_norm = (math.sqrt(math.pow(delta_x,2)+math.pow(delta_y,2)+math.pow(delta_z,2)))
+        if not delta_norm == 0:
+            return [delta_x/delta_norm, delta_y/delta_norm, delta_z/delta_norm]
+        return [0, 0, 0]
+        
+
 
     # ====== RUN LOOP =======
     # This is the main loop, notice that the PID reference signal (published on the goal topic) is calculated every deltaT second
@@ -110,9 +155,6 @@ class PositionHandler:
 
 if __name__ == "__main__":
     rospy.init_node("position_handler")
-    x = rospy.get_param("x")
-    y = rospy.get_param("y")
-    z = rospy.get_param("z")
 
-    posHandler = PositionHandler(x,y,z)
+    posHandler = PositionHandler()
     posHandler.run()
